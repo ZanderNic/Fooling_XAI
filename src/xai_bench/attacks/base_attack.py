2135bbf8f247596ef4b622fe7118593ea1cab742
seeeebaaaa <86500035+seeeebaaaa@users.noreply.github.com>
2026-01-10 22:09:49 +0100

    Error data insert (#4)
    * Working on Switch Column Attack
    
    * Dataset merge (#3)
    
    * changed datasets
    
    * added datasets
    
    * changed gitignore (datasets)
    
    ---------
    
    Co-authored-by: Thomas <gaillinger.thomas@gmail.com>
    
    * Changed path to dataset
    
    * tqdm
    
    ---------
    
    Co-authored-by: Thomas <gaillinger.thomas@gmail.com>

--- src/xai_bench/attacks/base_attack.py
+++ src/xai_bench/attacks/base_attack.py
@@ -8,6 +8,9 @@ class BaseAttack(ABC):
     def __init__(self, model: BaseModel):
         self.model = model
 
+    """
+    Will recieve ONE sample to generate an attack on
+    """
     @abstractmethod
     def generate(self, x: np.ndarray) -> np.ndarray:
         pass
