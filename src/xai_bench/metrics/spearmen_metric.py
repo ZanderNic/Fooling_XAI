097f54885915a8cc65c7da91564867d49ac32fb0
seeeebaaaa <86500035+seeeebaaaa@users.noreply.github.com>
2026-01-18 18:07:20 +0100

    'fixed' spearmen

--- src/xai_bench/metrics/spearmen_metric.py
+++ src/xai_bench/metrics/spearmen_metric.py
@@ -28,13 +28,10 @@ class SpearmanMetric(BaseMetric):
         Returns 1 minus the Spearman rank correlation between e1 and e2.
         Result ranges from 0 (identical ranking) to 2 (opposite ranking).
     """
-    def __init__(self, normalizer_mode="none"):
+    def __init__(self):
         super().__init__("spearman")
-        self.normalizer = ExplanationNormalizer(normalizer_mode)
 
     def _compute(self, e1: np.ndarray, e2: np.ndarray) -> float:
-        e1 = self.normalizer(e1)
-        e2 = self.normalizer(e2)
         r1 = np.argsort(-np.abs(e1))
         r2 = np.argsort(-np.abs(e2))
         res = spearmanr(r1, r2)
@@ -42,12 +39,16 @@ class SpearmanMetric(BaseMetric):
     
 
 if __name__ == "__main__":
-    exp_1 = np.array([0.4, 0.3, 0.2])
-    exp_2 = np.array([-0.4, 0.3, 0.2])
-    exp_3 = np.array([0.2, 0.3, 0.4])
+    exp_1 = np.array([1, 0, 0])
+    exp_2 = np.array([-1, 0, 0])
+    exp_3 = np.array([0, 1, 0])
 
     metric = SpearmanMetric()
-    print(metric.compute(exp_1, exp_2))
-    print(metric.compute(exp_1, exp_3))
-    print(metric.compute(exp_2, exp_3))
+    print(metric._compute(exp_1, exp_1))
+    print(metric._compute(exp_1, exp_2))
+    print(metric._compute(exp_1, exp_3))
+
+    # print(metric._compute(exp_1, exp_1))
+    # print(metric._compute(exp_1, exp_2))
+    # print(metric._compute(exp_1, exp_3))
 
